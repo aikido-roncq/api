34e1497fd2f5966f7f755f56a9d03bc8e73031d2
Ludal <44783088+iamludal@users.noreply.github.com>
2021-02-20 11:41:10 +0100

    refactor: ♻️ Imrove Model code (remove duplicates, ...)

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -11,133 +11,129 @@ use App\Utils;
 
 abstract class Model
 {
-    protected static string $pk;
-    protected static array $rules;
-    protected static array $labels;
+  protected static string $pk;
+  protected static array $rules;
+  protected static array $labels;
 
-    public function __construct(array $fields)
-    {
-        $v = Utils::validate($fields, static::$rules, static::$labels);
+  public function __construct(array $fields = [])
+  {
+    $v = Utils::validate($fields, static::$rules, static::$labels);
 
-        if (!$v->validate())
-            throw new ValidationException($v->errors());
+    if (!$v->validate())
+      throw new ValidationException($v->errors());
 
-        foreach ($fields as $key => $value)
-            $this->$key = $value;
-    }
+    foreach ($fields as $key => $value)
+      $this->$key = $value;
+  }
 
-    protected abstract static function make(array $fields = []);
+  protected abstract static function make(array $fields = []);
 
-    protected static function builder()
-    {
-        return new QueryBuilder(Factory::pdo());
-    }
+  private static function builder()
+  {
+    return new QueryBuilder(Factory::pdo());
+  }
 
-    protected static function table()
-    {
-        $className = explode('\\', static::class);
-        return strtolower(end($className));
-    }
+  private static function table()
+  {
+    $className = explode('\\', static::class);
+    return strtolower(end($className));
+  }
 
-    public static function find(string $key)
-    {
-        $row = self::builder()
-            ->select()
-            ->from(static::table())
-            ->where(static::$pk . " = :key")
-            ->setParam(':key', $key)
-            ->fetch();
+  public static function find(string $key)
+  {
+    $row = self::builder()
+      ->select()
+      ->from(static::table())
+      ->where(sprintf('%s = :key', static::$pk))
+      ->setParam(':key', $key)
+      ->fetch();
 
-        if (!$row)
-            throw new NotFoundException();
+    if (!$row)
+      throw new NotFoundException();
 
-        return new static($row);
-    }
+    return new static($row);
+  }
 
-    public static function all(array $conditions = [])
-    {
-        $rows = self::builder()
-            ->select()
-            ->from(static::table())
-            ->where(...$conditions)
-            ->fetchAll();
+  public static function all(array $conditions = [])
+  {
+    $rows = self::builder()
+      ->select()
+      ->from(static::table())
+      ->where(...$conditions)
+      ->fetchAll();
 
-        return array_map(function ($row) {
-            return new static($row);
-        }, $rows);
-    }
+    return array_map(function ($row) {
+      return new static($row);
+    }, $rows);
+  }
 
-    public static function orderBy(string $key = null, string $order = 'asc', array $conditions = [])
-    {
-        if (is_null($key))
-            $key = static::$pk;
+  public static function orderBy(string $key = null, string $order = 'asc', array $conditions = [])
+  {
+    if (is_null($key))
+      $key = static::$pk;
 
-        $rows = self::builder()
-            ->select()
-            ->from(static::table())
-            ->where(...$conditions)
-            ->orderBy($key, $order)
-            ->fetchAll();
+    $rows = self::builder()
+      ->select()
+      ->from(static::table())
+      ->where(...$conditions)
+      ->orderBy($key, $order)
+      ->fetchAll();
 
-        return array_map(function ($row) {
-            return new static($row);
-        }, $rows);
-    }
+    return array_map(function ($row) {
+      return new static($row);
+    }, $rows);
+  }
 
-    public static function create(array $fields = [])
-    {
-        $entity = static::make($fields);
-        $values = get_object_vars($entity);
+  public static function create(array $fields = [])
+  {
+    $instance = static::make($fields);
+    $values = get_object_vars($instance);
 
-        $created = self::builder()
-            ->insertInto(static::table())
-            ->values($values)
-            ->execute();
+    $created = self::builder()
+      ->insertInto(static::table())
+      ->values($values)
+      ->execute();
 
-        if (!$created)
-            throw new UnknownException();
+    if (!$created)
+      throw new UnknownException();
 
-        $pk = $values[static::$pk] ?? self::builder()->lastInsertId();
+    $pk = $values[static::$pk] ?? self::builder()->lastInsertId();
 
-        $entry = self::builder()
-            ->select()
-            ->from(static::table())
-            ->where(sprintf('%s = :key', static::$pk))
-            ->setParam(':key', $pk)
-            ->fetch();
+    return self::find($pk);
+  }
 
-        return new static($entry);
-    }
+  public static function delete(string $key)
+  {
+    $entry = self::find($key);
 
-    public static function delete(string $key)
-    {
-        $entry = self::find($key);
+    $deleted = self::builder()
+      ->deleteFrom(static::table())
+      ->where(sprintf('%s = :key', static::$pk))
+      ->setParam(':key', $key)
+      ->execute();
 
-        self::builder()
-            ->deleteFrom(static::table())
-            ->where(static::$pk . ' = :key')
-            ->setParam(':key', $key)
-            ->execute();
+    if (!$deleted)
+      throw new UnknownException();
 
-        return $entry;
-    }
+    return $entry;
+  }
 
-    public static function update(string $key, array $new_fields)
-    {
-        $fields = get_object_vars(self::find($key));
-        $updated = array_merge($fields, $new_fields);
-        $new_entry = new static($updated);
+  public static function update(string $key, array $new_fields)
+  {
+    $fields = get_object_vars(self::find($key));
+    $updated = array_merge($fields, $new_fields);
+    $new_instance = new static($updated);
 
-        $executed = self::builder()
-            ->update(static::table())
-            ->set($updated)
-            ->where(static::$pk . ' = :key')
-            ->setParam(':key', $key)
-            ->execute();
+    $executed = self::builder()
+      ->update(static::table())
+      ->set($updated)
+      ->where(sprintf('%s = :key', static::$pk))
+      ->setParam(':key', $key)
+      ->execute();
 
-        if (!$executed)
-            throw new UnknownException();
+    if (!$executed)
+      throw new UnknownException();
 
-        return $new_entry;
-    }
+    return $new_instance;
+  }
 }

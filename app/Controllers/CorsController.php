85c72b45fc40105847af2e8f1be2210cc5430ea0
Ludal <44783088+iamludal@users.noreply.github.com>
2021-03-05 19:50:06 +0100

    refactor: ♻️ Move CorsController headers to CorsMiddleware

--- app/Controllers/CorsController.php
+++ app/Controllers/CorsController.php
@@ -11,8 +11,6 @@ class CorsController extends Controller
   #[Route('/{route:.*}', 'OPTIONS')]
   public function preflight(Request $req, Response $res)
   {
-    return $res
-      ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-      ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    return $res;
   }
 }
--- app/Middlewares/CorsMiddleware.php
+++ app/Middlewares/CorsMiddleware.php
@@ -12,6 +12,8 @@ class CorsMiddleware
     $res = $handler->handle($req);
 
     return $res
-      ->withHeader('Access-Control-Allow-Origin', '*');
+      ->withHeader('Access-Control-Allow-Origin', '*')
+      ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+      ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   }
 }

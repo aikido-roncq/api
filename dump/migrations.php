26be660e923ef55f9d3b6770f99da042e2c8532a
Ludal <ludovic.chombeau@gmail.com>
2021-07-20 21:11:59 +0200

    feat: update migrations script

--- dump/migrations.php
+++ dump/migrations.php
@@ -13,19 +13,61 @@ Dotenv::createImmutable(dirname(__DIR__))->load();
 
 $pdo = Factory::pdo();
 
-$scannedDir = scandir($migrationsDir, SCANDIR_SORT_ASCENDING);
-$migrations = array_filter($scannedDir, function ($entry) use ($baseDir) {
-  return !is_dir("$baseDir/$entry");
-});
+function get_migrations_history(): array
+{
+  global $pdo;
 
-Logger::debug("starting migrations in directory $migrationsDir");
+  $stmt = $pdo->prepare("SELECT script FROM migrations ORDER BY script");
+  $stmt->execute();
+  $scripts = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
-foreach ($migrations as $migration) {
-  $query = file_get_contents("$migrationsDir/$migration");
+  return $scripts;
+}
 
-  if ($pdo->exec($query)) {
-    Logger::info("OK: $migration");
-  } else {
-    Logger::error("FAILED: $migration");
+function scan_migrations(): array
+{
+  global $migrationsDir;
+  $migrations = array_map(function ($file) {
+    return basename($file);
+  }, glob("$migrationsDir/*.sql"));
+  return $migrations;
+}
+
+function run_migrations(array $migrations): void
+{
+  global $pdo;
+  global $migrationsDir;
+
+  if (empty($migrations)) {
+    Logger::info("No migrations to run.");
+    return;
+  }
+
+  Logger::debug("Starting migrations of directory: $migrationsDir");
+
+  foreach ($migrations as $migration) {
+    $query = file_get_contents("$migrationsDir/$migration");
+
+    try {
+      $pdo->exec($query);
+      save_migration($migration);
+      Logger::info("OK: $migration");
+    } catch (PDOException $e) {
+      Logger::error("Error running migration: $migration ({$e->getMessage()}");
+    }
   }
 }
+
+function save_migration(string $migration)
+{
+  global $pdo;
+
+  $stmt = $pdo->prepare("INSERT INTO migrations (script) VALUES (?)");
+  $stmt->execute([$migration]);
+}
+
+
+$migrationsHistory = get_migrations_history();
+$scannedMigrations = scan_migrations();
+$migrationsToRun = array_diff($scannedMigrations, $migrationsHistory);
+run_migrations($migrationsToRun);
